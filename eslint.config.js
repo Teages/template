2267f932e3d9b150a3dd02a283ae168a0be286bf
Teages <Teages@teages.xyz>
2024-08-24 21:56:05 +0800

    chore: using workspace to run playground

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,6 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: [
-    '__generated__',
-    'node_modules',
-    'coverage',
-    'dist',
-  ],
   rules: {
     curly: ['error', 'all'],
   },
