2aa5b5c37a6746c4e93fe6710ece01b881cbfe25
Teages <Teages@teages.xyz>
2024-12-10 01:51:28 +0800

    chore: speed up dts generation

--- build.config.ts
+++ build.config.ts
@@ -31,13 +31,13 @@ export default defineBuildConfig({
   ],
 
   hooks: {
-    'mkdist:entry:build': async (_ctx, _entries, { writtenFiles }) => {
+    'mkdist:entry:build': (_ctx, _entries, { writtenFiles }) => {
       const declarations = writtenFiles.filter(file => file.endsWith('.d.ts'))
       // copy declarations to '.d.mts' and '.d.cts'
       for (const declaration of declarations) {
         const name = declaration.replace(/.d.ts$/, '')
-        await fs.copyFile(declaration, `${name}.d.mts`)
-        await fs.copyFile(declaration, `${name}.d.cts`)
+        fs.copyFile(declaration, `${name}.d.mts`)
+        fs.copyFile(declaration, `${name}.d.cts`)
       }
     },
   },
