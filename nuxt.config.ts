d424d10af6292cd2b022b48ad44d10c60d96c263
Teages <Teages@teages.xyz>
2025-06-08 22:08:46 +0800

    feat: use nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  future: { compatibilityVersion: 4 },
+
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui-pro',
+  ],
+
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
 
   eslint: {
     config: { standalone: false },
+  },
+
+  icon: {
+    provider: 'iconify',
   },
 })
---
+++ server/api/index.get.ts
@@ -0,0 +1,6 @@
+export default defineEventHandler(async () => {
+  return {
+    success: true,
+    message: 'Hello World',
+  }
+})
