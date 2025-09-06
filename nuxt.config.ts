af08f62c8db457d87cfcef1fd2b6c17a8ae63f66
Teages <Teages@teages.xyz>
2025-09-07 02:09:02 +0800

    chore(deps): update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  future: { compatibilityVersion: 4 },
 
   css: ['~/assets/css/main.css'],
 
-  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@vueuse/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt'],
 
   devtools: { enabled: true },
 
