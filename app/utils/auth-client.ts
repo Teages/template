4688d490a42ed961be12639eed0c1a45640a50eb
Teages <Teages@teages.xyz>
2026-08-14 17:01:12 +0800

    refactor: drop unused auth client re-exports

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,7 +1,3 @@
 import { createAuthClient } from 'better-auth/vue'
 
 export const authClient = createAuthClient()
-
-export const { signIn, signUp, signOut } = authClient
-
-export const { useSession } = authClient
