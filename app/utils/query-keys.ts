361eeb0e44fb60b7da3a7104a322f317a91ab69f
Teages <Teages@teages.xyz>
2026-08-17 11:24:51 +0800

    refactor: remove the frontend app utils

--- app/utils/query-keys.ts
+++
@@ -1,7 +0,0 @@
-export const AUTH_SESSION_QUERY_KEY = ['auth', 'session'] as const
-
-export const COUNT_QUERY_KEYS = {
-  graphql: ['count-events', 'graphql'] as const,
-  rest: ['count-events', 'rest'] as const,
-  trpc: ['count-events', 'trpc'] as const,
-}
