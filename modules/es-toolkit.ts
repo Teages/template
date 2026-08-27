75d67c1ab9037dc9b4b319e22a9aca1b747b8520
Teages <Teages@teages.xyz>
2026-08-27 17:37:07 +0800

    fix: prevent duplicate imports in es-toolkit module

--- modules/es-toolkit.ts
+++ modules/es-toolkit.ts
@@ -1,12 +1,12 @@
 import { addImportsSources, defineNuxtModule } from 'nuxt/kit'
 
 const importPkgList = [
+  'es-toolkit/promise',
   'es-toolkit/array',
   'es-toolkit/function',
   'es-toolkit/math',
   'es-toolkit/object',
   'es-toolkit/predicate',
-  'es-toolkit/promise',
   'es-toolkit/string',
   'es-toolkit/util',
 ]
@@ -17,14 +17,18 @@ export default defineNuxtModule({
     configKey: 'esToolkit',
   },
   async setup(_options) {
+    const seen = new Set<string>()
     for (const pkg of importPkgList) {
-      const imports = await listAvaliableImports(pkg)
+      const imports = await listAvaliableImports(pkg, seen)
       addImportsSources({ from: pkg, imports })
     }
   },
 })
 
-async function listAvaliableImports(pkg: string) {
+async function listAvaliableImports(pkg: string, seen: Set<string>) {
   const imports = await import(pkg)
-  return Object.keys(imports).filter(key => !key.startsWith('_'))
+  const names = Object.keys(imports).filter(key => !key.startsWith('_') && !seen.has(key))
+  for (const name of names)
+    seen.add(name)
+  return names
 }
